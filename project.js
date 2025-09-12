284368103dab8e18773fa1fc0c10b142500061d8
sebz <sebzcode55@gmail.com>
2025-09-13 01:21:53 +0530

    completed (version 1)

--- project.js
+++ project.js
@@ -97,14 +97,52 @@ const printRows = (rows) => {
         }
         console.log(rowString);
     }
-}
+};
 
-let balance = deposit();
-//const depositAmount = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance,numberOfLines);
-const reels = spin();
-const rows = transpose(reels);
-//console.log(reels);
-//console.log(rows);
-printRows(rows);
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+    for(let row=0; row<lines; row++){
+        const symbols = rows[row];
+        let allSame = true;
+        for(const symbol of symbols){
+            if(symbol != symbols[0]){
+                allSame = false;
+                break;
+            }
+        }
+        if(allSame){
+            winnings += bet * SYMBOL_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+};
+
+const game = () => {
+    let balance = deposit();
+    while(true){
+        console.log("Your Balance : "+ balance);
+
+        const numberOfLines = getNumberOfLines();
+        const bet = getBet(balance,numberOfLines);
+
+        balance -= bet * numberOfLines;
+
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows, bet, numberOfLines);
+
+        balance += winnings;
+
+        console.log("You won, $" + winnings.toString());
+        console.log("Your Balance : "+ balance);
+
+        if(balance <= 0){
+            console.log("You ran out of money!");
+            break;
+        }
+        const playAgain = prompt("Do you want to play again (y/n)? ");
+        if(playAgain != "y") break;
+    }
+};
+game();
