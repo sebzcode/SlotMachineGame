b05447d508ca974aa09cd040fd34b1bade5190f5
sebz <sebzcode55@gmail.com>
2025-09-13 00:47:19 +0530

    spin done

--- project.js
+++ project.js
@@ -1,8 +1,18 @@
 //1 Deposit money
 //2 Determine the number of lines to bet on
 //3 Collect a bet amount
+//4 spin the slot machine
+//5 check if the user won
+//6 give the user their winnings
+//7 play again
 
 const prompt = require("prompt-sync")();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = { A:2, B:4, C:6, D:8};
+const SYMBOL_VALUES = {A:5, B:4, C:3, D:2};
 
 const deposit = () => {
     while(true){
@@ -43,7 +53,58 @@ const getBet = (balance,lines) => {
     }
 };
 
+const spin = () => {
+    const symbols = [];
+    for(const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        for(let i=0; i<count; i++){
+            symbols.push(symbol)
+        }
+    }
+
+    const reels = [];
+    for(let i=0; i<COLS; i++){
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for(let j=0; j<ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+    for(let i=0; i<ROWS; i++){
+        rows.push([]);
+        for(let j=0; j<COLS; j++){
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+};
+
+const printRows = (rows) => {
+    for(const row of rows){
+        let rowString = "";
+        for(const [i, symbol] of row.entries()){
+            rowString += symbol;
+            if(i != row.length - 1){
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+}
+
 let balance = deposit();
 //const depositAmount = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance,numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+//console.log(reels);
+//console.log(rows);
+printRows(rows);
